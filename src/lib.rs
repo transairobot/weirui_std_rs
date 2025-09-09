c10f57a6005262ae892db9fc7d1ac21a1594c2e1
Your Real Name <you@example.com>
2025-09-09 16:37:55 +0800

    up

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,12 @@
 pub mod host;
 pub mod utils;
-
-use host::host_func::*;
-
 #[macro_export]
 macro_rules! println {
     () => {
-        host::host_func::write_console("\n").unwrap();
+        weirui_std_rs::host::host_func::write_console("\n").unwrap();
     };
     ($($arg:tt)*) => {{
         let x = core::format_args!($($arg)*);
-        host::host_func::write_console(&format!("{}\n", x)).unwrap();
+        weirui_std_rs::host::host_func::write_console(&format!("{}\n", x)).unwrap();
     }};
 }
